d82f87cd186c64739ffc7c5ee45f36635e1e107f
chaahn <chaahn@gmarket.com>
2024-06-20 11:11:52 +0900

    강의 중 추가코드 및 수정코드 반영

--- src/main/java/com/kb/shop/domain/Seller.java
+++ src/main/java/com/kb/shop/domain/Seller.java
@@ -10,11 +10,12 @@ public class Seller {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String sellerName; // abcFashionMall
     @Enumerated(EnumType.STRING)
     private SellerGrade sellerGrade;
+    private Double margin; // 3
+    private String policy; // unlimit
+    private String category; // fashion
 
-    private Double margin;
-
-    private String policy;
-    private String category;
+    // insert into seller (id, sellerName, sellerGrade, margin, policy, category) values (1, 'abcFa.....);
 }
